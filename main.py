6108a7fe0a3d15d9b7b4db0438e09d4d5f0bb954
beautyyu <beautyyuyanli@outlook.com>
2019-11-06 12:23:59 +0800

    add datebase

--- main.py
+++ main.py
@@ -9,9 +9,21 @@ def convert(name):
     else:
         img.thumbnail((max_size, max_size))
     img.save('midsize/' + name.split('.')[0] + '_md.jpg')
+# main
+with open('datebase.qwq', 'r') as f:
+    datebase = f.read().split(';')
+if datebase[-1] == '':
+    datebase.pop()
 for i, j, k in os.walk('origin'):
     for l in k:
+        if l.split('.')[0] in datebase:
+            continue
         try:
             convert(l)
+            datebase.append(l.split('.')[0])
+            print(l)
         except:
             print('convert failed: ' + l)
+with open('datebase.qwq', 'w') as f:
+    for i in datebase:
+        f.write(i + ';')
